676ed3b8371fcfc0b1afab9ee7b1678567748668
12 <gyfo6474@naver.com>
2026-02-06 10:40:26 +0900

    끝말잇기

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WordGame from './components/WordGame.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <WordGame startWord='학교'/>
+    </div>
+  )
+}
+
+export default App
